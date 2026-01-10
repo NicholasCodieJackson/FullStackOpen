2a16f4c1dc75b5168e29ca8df28358096d97cff8
Nick <ncjackson0110@proton.me>
2026-01-10 13:26:08 +0000

    1.5 refactored to hold all course info in an object then pass specific data down and destructure it

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,59 +1,60 @@
 const App = () => {
-  const course = 'Half stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
 const Header = ({course}) => {
-  
   return (
     <h1>{course}</h1>
   )
 }
 
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={parts[0]}/>
-      <Part part={parts[1]}/>
-      <Part part={parts[2]}/>
-          
-    </div>       
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+
+    </div>
   )
 }
 
-const Part = ({part}) => {
-  return(
+const Part = ({ part}) => {
+  return (
     <p>
       {part.name} {part.exercises}
     </p>
   )
 }
 
-const Total = ({parts}) => {
+const Total = ({ parts }) => {
   return (
-    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>  
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
   )
-  
+
 }
 export default App
