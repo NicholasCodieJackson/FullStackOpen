77b16f73913f53a8a7367884faef2fdda1764c41
Nick <ncjackson0110@proton.me>
2026-01-13 12:33:17 +0000

    1.8 refactored statistics into own component with conditional rendering

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,33 @@ import { useState } from 'react'
 
 const Button = (props) => <button onClick={props.onClick}>{props.name}</button>
 
-function App() {
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+
+  if(total === 0){
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>NO FEEDBACK DATA AVAILABLE</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad) / total}</p>
+      <p>positive {((good) / total) * 100} %</p>
+    </div>
+  )
+}
+
+const App = () =>{
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -14,14 +40,7 @@ function App() {
       <Button onClick={() => setNeutral(neutral + 1)} name="neutral"/>
       <Button onClick={() => setBad(bad + 1)} name="bad"/>
 
-      <h2>Statistics</h2>
-
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {((good) / (good + neutral + bad)) * 100}</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
