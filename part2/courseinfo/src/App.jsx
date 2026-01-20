dbda810d4cf965581201d3ac76747550e8ad53b4
Nick <ncjackson0110@proton.me>
2026-01-20 18:12:09 +0000

    2.4 made it so that as our courses data grows the App can render them all

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,35 +1,54 @@
 import Course from './components/Course'
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'test',
-        exercises: 5,
-        id: 4
-      }
-    ]
-  }
+  const course = [
+    {
+      id: 1,
+      name: 'Half stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'test',
+          exercises: 5,
+          id: 4
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middleware',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
 
   return (
-    <Course key={course.id} course={course}/>
+    course.map((course) => <Course key={course.id} course={course}/>)
   )
 }
 
