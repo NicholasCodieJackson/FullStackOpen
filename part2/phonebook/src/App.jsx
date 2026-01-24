41f8e701c2edcae6d62dd330f3d2a212da533eff
Nick <ncjackson0110@proton.me>
2026-01-24 23:58:48 +0000

    2.6 added functionality to add names to the persons state and display

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,23 +2,53 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      id: 1
+    },
+    { 
+      name: 'Ada Lovelace',
+      id: 2
+    }
   ]) 
   const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    const newPerson = {
+      name: newName,
+      id: persons.length + 1
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person => <p key={person.id}>{person.name}</p>)}
+
+
+
+        debug: {newName === '' ? 'newName state is empty' : newName}
+      </div>
     </div>
   )
 }
