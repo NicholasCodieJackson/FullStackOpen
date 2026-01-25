482413893efc25eaab67aff36f09057f0f070f62
Nick <ncjackson0110@proton.me>
2026-01-25 02:20:19 +0000

    2.9 Add case-sensitive filter by name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameSearch, setNameSearch] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -11,6 +17,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleNameFilterChange = (event) => {
+    setNameSearch(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -22,6 +32,7 @@ const App = () => {
     } 
 
     if (newName === '' || newNumber === ''){
+      alert(`Please include both name and number`) 
       return
     }
 
@@ -39,6 +50,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          Filter by name: <input value={nameSearch} onChange={handleNameFilterChange} />
+      </div>
+
+      <h2>Add new entry</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -53,9 +69,9 @@ const App = () => {
 
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.id}>{person.name} - {person.number}</p>)}
-
-
+        {persons.filter(person => 
+          person.name.toLowerCase().includes(nameSearch.toLowerCase())
+        ).map((person) => <p key={person.id}>{person.name} - {person.number}</p>)}
 
         {persons.length === 0 ? 'No people in phone book' : ''}
       </div>
